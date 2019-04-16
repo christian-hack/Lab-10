baa39470cb9f54a6546d51d10a54a7f897eda0f5
Christian Hackelman <cphackelman@ou.edu>
2019-04-16 18:58:37 -0500

    square draw location settings

--- Lab 10/src/Square.java
+++ Lab 10/src/Square.java
@@ -5,5 +5,14 @@ public class Square extends Rectangle{
 
 	public Square(Point pointUL, int width, Color color, boolean filled) {
 		super(pointUL, width, width, color, filled);
+		location = new Point[4];
+		location[0] = pointUL;
+		
+		Point ul = new Point(pointUL.x, pointUL.y + width);
+		Point ur = new Point(pointUL.x + width, pointUL.y + width);
+		Point ll = new Point(pointUL.x + width, pointUL.y);
+		location[1] = ul;
+		location[2] = ur;
+		location[3] = ll;
 	}
 }
