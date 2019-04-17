48b08f76aa262517121726f7d09e3ddd7ee6fc7a
Christian Hackelman <cphackelman@ou.edu>
2019-04-16 19:01:54 -0500

    triangle draw location settings

--- Lab 10/src/RightTriangle.java
+++ Lab 10/src/RightTriangle.java
@@ -5,5 +5,12 @@ public class RightTriangle extends Triangle{
 
 	public RightTriangle(Point pointRA, int base, int height, Color color, boolean fill) {
 		super(pointRA, color, fill);
+		location = new Point[3];
+		location[0] = pointRA;
+		
+		Point upAng = new Point(pointRA.x, pointRA.y + height);
+		Point adjAng = new Point(pointRA.x + base, pointRA.y);
+		location[1] = upAng;
+		location[2] = adjAng;
 	}
 }
