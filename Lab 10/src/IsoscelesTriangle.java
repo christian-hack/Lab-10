76d383114241ccc7b8cc94f32540c26e3cf88f5b
Christian Hackelman <cphackelman@ou.edu>
2019-04-16 19:14:00 -0500

    isosceles triangle draw settings

--- Lab 10/src/IsoscelesTriangle.java
+++ Lab 10/src/IsoscelesTriangle.java
@@ -5,6 +5,13 @@ public class IsoscelesTriangle extends Triangle{
 	
 	public IsoscelesTriangle(Point pointLB, int base, int height, Color color, boolean fill) {
 		super(pointLB, color, fill);
+		location = new Point[3];
+		location[0] = pointLB;
+		
+		Point two = new Point((int)(pointLB.x + .5*base), pointLB.y + height);
+		Point three = new Point(pointLB.x + base, pointLB.y);
+		location[1] = two;
+		location[2] = three;
 	}
 
 }
