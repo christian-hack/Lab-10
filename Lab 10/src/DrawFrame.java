85d3f1902aebbbeda27b739b5f69eb0a9cd91b7d
Christian Hackelman <cphackelman@ou.edu>
2019-04-16 19:15:09 -0500

    removed unused imports

--- Lab 10/src/DrawFrame.java
+++ Lab 10/src/DrawFrame.java
@@ -1,12 +1,7 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Point;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class DrawFrame extends JFrame{
 	
